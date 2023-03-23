f6daa38a9dc8b914649164e9a3146e0f8eddfef5
tejutalekar <128734585+tejutalekar@users.noreply.github.com>
2023-03-23 23:11:12 +0530

    Create assignment1.js

---
+++ assignment1.js
@@ -0,0 +1,10 @@
+var arr= [91, 33, 11, 5, 67, 3, 55, 78, 50, 48, 99];
+
+for (let i=0; i < arr.length; i++)
+
+    {
+        if(arr[i] % 3 == 0)
+        {
+            console.log(arr[i]);
+        }
+    }
