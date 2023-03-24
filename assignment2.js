3e542d6a643bf0ca1d84ac415fefa17c6cf866e0
tejutalekar <128734585+tejutalekar@users.noreply.github.com>
2023-03-24 10:22:15 +0530

    Create assignment2.js

---
+++ assignment2.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+        const date =new Date();     
+        const a =date.toDateString();
+        const time=date.toLocaleTimeString();
+        console.log("Date:"+ a);
+        console.log("Time:"+ time);
+    
+
+
+    </script>
+</head>
+<body>
+    <!-- welcome to javascript bootcamp..... Teju -->
+<button onclick="display()">click me</button>
+</button></body>
+</html>
